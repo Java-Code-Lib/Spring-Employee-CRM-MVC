9db0e9c93f8cc77505dddffcfeae9dfff986d6b0
kevinstradtman <stradtkt@outlook.com>
2023-04-12 17:59:29 -0400

    Initial Commit

---
+++ src/main/java/com/stradtkt/employeecrmmvc/EmployeeCrmmvcApplication.java
@@ -0,0 +1,13 @@
+package com.stradtkt.employeecrmmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrmmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrmmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stradtkt/employeecrmmvc/EmployeeCrmmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stradtkt.employeecrmmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrmmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
